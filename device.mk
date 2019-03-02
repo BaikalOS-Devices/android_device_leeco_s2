46697230576b6b0246bc1abd4afcba811ab3357e
dudkin-2005 <dudkin-2005@yandex.ru>
2019-03-03 04:15:41 +0700

    s2: build keystore from source

--- device.mk
+++ device.mk
@@ -259,6 +259,14 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf \
     $(LOCAL_PATH)/configs/msm_irqbalance_little_big.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance_little_big.conf
 
+# Keystore
+PRODUCT_PACKAGES += \
+    keystore.msm8952
+
+# LeEco Parts
+PRODUCT_PACKAGES += \
+    LeEcoParts
+
 # Libshims
 PRODUCT_PACKAGES += \
     libshims_camera \
