f2b0cbfa01f82151311d8aa6aab12a97c3681072
dudkin-2005 <dudkin-2005@yandex.ru>
2019-03-03 04:06:13 +0700

    added perfomance profiles

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -51,6 +51,14 @@ LOCAL_SRC_FILES		:= etc/ueventd.qcom.rc
 LOCAL_MODULE_PATH	:=  $(TARGET_OUT_VENDOR)
 include $(BUILD_PREBUILT)
 
+include $(CLEAR_VARS)
+LOCAL_MODULE            := init.spectrum.rc
+LOCAL_MODULE_TAGS       := optional eng
+LOCAL_MODULE_CLASS      := ETC
+LOCAL_SRC_FILES         := etc/init.spectrum.rc
+LOCAL_MODULE_PATH       := $(TARGET_OUT_VENDOR_ETC)/init/hw
+include $(BUILD_PREBUILT)
+
 # Configuration scripts
 
 include $(CLEAR_VARS)
@@ -71,3 +79,4 @@ LOCAL_SRC_FILES       := sbin/chargeonlymode
 LOCAL_MODULE_TAGS     := optional
 LOCAL_MODULE_CLASS    := EXECUTABLES
 include $(BUILD_PREBUILT)
+
